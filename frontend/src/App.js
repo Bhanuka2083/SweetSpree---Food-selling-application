0cec97cc58f6c3adaf2992e90c0057a70c7bc9c2
T M B M Thennakoon <bmaheesha2083@gmail.com>
2026-06-04 11:07:17 +0530

    tailwind

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import {
+  Button,
+  Avatar,
+  Dropdown,
+  DropdownDivider,
+  DropdownHeader,
+  DropdownItem,
+  Navbar,
+  NavbarBrand,
+  NavbarCollapse,
+  NavbarLink,
+  NavbarToggle,
+} from 'flowbite-react';
 import './App.css';
 
 // Import your page components
@@ -9,20 +22,33 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import UserProfile from './pages/UserProfile';
 
+import logo from './assets/logo.png';
+
+
+
+
 function App() {
   return (
     <BrowserRouter>
     <div className="app-container">
       {/* This Navigation bar stays on the screen on every page */}
-      <nav className="navbar">
-        <div className="logo">SweetSpree 🍰</div>
-        <div className="nav-links">
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-          <Link to="/contact">Contact</Link>
-          <Link to="/profile">Profile</Link>
-        </div>
-      </nav>
+       <Navbar className="navbar" class="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
+        
+         <div className="logo" class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+           <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
+               <img src={logo} width='50px' height='50px' class="h-7" alt="SweetSpree" />
+               <span class="self-center text-x3 text-heading font-semibold whitespace-nowrap">SweetSpree</span>
+           </a>
+         </div>
+
+         <div className="nav-links">
+           <Link to="/">Home</Link>
+           <Link to="/about">About</Link>
+           <Link to="/contact">Contact</Link>
+           <Link to="/profile">Profile</Link>
+         </div>
+       </Navbar>
+       
 
       {/* The Routes block swaps out the component based on the URL */}
       <main className="content-area">
@@ -37,23 +63,10 @@ function App() {
       </main>
     </div>
 
-    
-
-
-
-
-
-
-
-
-    
-
-
-
-
 
     </BrowserRouter>
   );
 }
 
 export default App;
+
